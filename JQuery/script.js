edbe2ea2fc35fcb29763a1cac85025abd5b44d88
Jprzg <97595762+Jprzg@users.noreply.github.com>
2022-01-23 23:12:56 +0100

    Add files via upload

---
+++ JQuery/script.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 15,
+    nav: false,
+    autoplay: true,
+    autoplayTimeout: 1500,
+    stagePadding: 50,
+    dots: false,
+    responsive: {
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:6
+        }
+    }
+})
